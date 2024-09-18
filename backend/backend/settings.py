2eeef00b57dd4ffce7b5d6ea5149a4481d247857
Dmitry Shuvalov <dshuval@yandex.ru>
2024-09-18 20:07:44 +0300

    flake8 checked

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,4 @@
 import os
-
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,9 +72,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
-
-
-
 
 # DATABASES = {
 #     'default': {
